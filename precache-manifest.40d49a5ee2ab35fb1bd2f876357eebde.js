8d35aaaf420428496aa92cc307d30648011753fc
Ampex <kasperski.blazej@gmail.com>
2020-01-02 13:52:58 +0100

    Updates

---
+++ precache-manifest.40d49a5ee2ab35fb1bd2f876357eebde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666a757b358c069ac3a11ac9182e42e1",
+    "url": "/messageBoard/index.html"
+  },
+  {
+    "revision": "d7e0577814072f770182",
+    "url": "/messageBoard/static/css/main.91198033.chunk.css"
+  },
+  {
+    "revision": "f3895553320b26ddec77",
+    "url": "/messageBoard/static/js/2.3f072b21.chunk.js"
+  },
+  {
+    "revision": "d7e0577814072f770182",
+    "url": "/messageBoard/static/js/main.c1dec3e3.chunk.js"
+  },
+  {
+    "revision": "892ffc2a9eca1bd856f3",
+    "url": "/messageBoard/static/js/runtime-main.d204f4ee.js"
+  },
+  {
+    "revision": "cf158486589dfd150bcffc78f448e3eb",
+    "url": "/messageBoard/static/media/bg.cf158486.svg"
+  }
+]);
