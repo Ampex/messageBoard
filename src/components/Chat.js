ebd177470be3758df63358a2313c57c37490de15
Ampex <kasperski.blazej@gmail.com>
2020-01-06 19:06:37 +0100

    new chat, optimize code

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { IconButton, Grow, TextField, Avatar, Tooltip } from '@material-ui/core'
+import {
+  IconButton,
+  Grow,
+  Avatar,
+  TextField,
+  Tooltip,
+  Typography,
+  Divider
+} from '@material-ui/core'
 import ChatIcon from '@material-ui/icons/Chat'
 import SendIcon from '@material-ui/icons/Send'
 import CancelIcon from '@material-ui/icons/Cancel'
 
 export default function Chat() {
   const [isActive, setActive] = useState(false)
+  const [isError, setError] = useState(false)
   const [isEmpty, setEmpty] = useState('')
+  const [isMessage, setMessages] = useState([])
 
   const LightTooltip = withStyles(theme => ({
     tooltip: {
@@ -26,12 +36,28 @@ export default function Chat() {
     }
   }))(isActive ? SendIcon : ChatIcon)
 
-  const handleActive = e => {
+  const handleActive = () => {
     setActive(true)
   }
-  const handleClear = e => {
-    setEmpty('')
+  const handleSubmit = () => {
+    const data = new Date().toLocaleTimeString()
+    if (isEmpty.length > 0) {
+      setMessages([...isMessage, { message: isEmpty, time: data }])
+      setEmpty('')
+      setError(false)
+    } else setError(true)
   }
+  const handleChange = e => {
+    setEmpty(e.target.value)
+  }
+
+  const messages = isMessage.map((element, i) => (
+    <div key={i}>
+      <code>{element.time} / as Guest</code>
+      <p>{element.message}</p>
+      <Divider style={{ margin: '10px 0' }} />
+    </div>
+  ))
 
   return (
     <div className='chat'>
@@ -41,7 +67,7 @@ export default function Chat() {
             <IconButton
               className='chat-icon'
               color='secondary'
-              onClick={isActive ? handleClear : handleActive}
+              onClick={isActive ? handleSubmit : handleActive}
             >
               <BiggerIcon />
             </IconButton>
@@ -52,18 +78,26 @@ export default function Chat() {
         <div className='chat chat-window bg'>
           <div className='close'>
             <Avatar>G</Avatar>
+            <Typography color='primary' variant='button'>
+              Messages : {isMessage.length}
+            </Typography>
             <LightTooltip placement='top' title='Close button'>
               <IconButton onClick={() => setActive(!isActive)}>
                 <CancelIcon />
               </IconButton>
             </LightTooltip>
           </div>
+          <Typography color='primary' variant='subtitle2'>
+            {isMessage.length > 0 ? messages : false}
+          </Typography>
           <TextField
             style={{ margin: '15px 0 0 0' }}
             value={isEmpty}
-            onChange={e => setEmpty(e.target.value)}
+            onChange={handleChange}
             fullWidth
             multiline
+            error={isError}
+            helperText={isError && 'Must be filled'}
             variant='standard'
             margin='dense'
             placeholder='Your message'
